c45467185c1a88bfbc315ec796b940527d4fb4eb
hhhsdi <tomasklodettmorell@gmail.com>
2025-05-28 10:20:21 +0300

    Add files via upload

---
+++ ManufacturerService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ManufacturerService
+{
+    private readonly IManufacturerRepository _repository;
+
+    public ManufacturerService(IManufacturerRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public List<Manufacturer> GetAllManufacturers()
+    {
+        return _repository.GetAllManufacturers();
+    }
+
+    public Manufacturer GetManufacturerById(int id)
+    {
+        if (id <= 0) throw new ArgumentException("Invalid ID");
+        return _repository.GetManufacturerById(id) ?? throw new KeyNotFoundException("Manufacturer not found");
+    }
+}
